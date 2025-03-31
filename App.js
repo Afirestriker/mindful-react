70000b6b7f00d5d1fb408c0ae0a6c4c612154641
Ashish Agrawal <agrawalashish.in@gmail.com>
2025-03-31 19:27:37 +0530

    Ep01 Inception Part-04 Complete
    Creating nested div structure using core react funciton React.createElement and understanding the need for JSX

--- App.js
+++ App.js
@@ -1,5 +1,41 @@
-const heading = React.createElement('customTag', {'id': 'heading', 'name': 'Ashish'}, 'Hello World From React!');
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *    <h1></h1>
+ *    <h1></h1>
+ *  </div>
+ *  <div id="child2">
+ *    <h1></h1>
+ *    <h1></h1>
+ *  </div>
+ * </div>
+ *
+ * ReactElement is an Object => which convert the object into an HTML tag which the Browser's understand
+ * ReactElement(Object) => HTML(Browser Understand)
+ */
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child", key: "child1" }, [
+        React.createElement("h1", { key: "key1" }, "I am a h1 tag!"),
+        React.createElement(
+            "h2",
+            { key: "key2" },
+            "I am a h2 tag under same child node!"
+        ),
+    ]),
+    React.createElement("div", { id: "child2", key:"child2" }, [
+        React.createElement("h1", { key: "key1" }, "I am a h1 tag!"),
+        React.createElement(
+            "h2",
+            { key: "key2" },
+            "I am a h2 tag under same child node!"
+        ),
+    ]),
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(parent); // object
 
-root.render(heading);
+// JSX?
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
