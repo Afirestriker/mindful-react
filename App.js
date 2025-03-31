b8b4bfbba96af97ef4aa58520b71a5c9b5bb15a8
Ashish Agrawal <agrawalashish.in@gmail.com>
2025-03-31 19:27:06 +0530

    Ep01 Inception Part-04 Complete
    Creating nested div structure using core react funciton React.createElement and understanding the need for JSX

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
-const heading = React.createElement('customTag', {'id': 'heading', 'name': 'Ashish'}, 'Hello World From React!');
+/**
+ * <div>
+ *  <div>
+ *    <h1></h1>
+ *  </div>
+ * </div>
+ *
+ * ReactElement is an Object => which convert the object into an HTML tag which the Browser's understand
+ * ReactElement(Object) => HTML(Browser Understand)
+ */
+const heading = React.createElement('h1', {id: 'heading', name: 'Ashish'}, 'Hello World From React!');
+const children = React.createElement('div', {id: 'child'}, heading);
+const parent = React.createElement('div', {id: 'parent'}, children);
+
+console.log(parent) // object
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(parent);
