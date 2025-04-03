d0fb470d3fe465aa7a3cd5b11788d71b1ee5fe4d
Ashish Agrawal <agrawalashish.in@gmail.com>
2025-04-03 10:02:31 +0530

    feat: clear tricky questions and concept clarity

--- App.js
+++ App.js
@@ -13,22 +13,12 @@
  * ReactElement is an Object => which convert the object into an HTML tag which the Browser's understand
  * ReactElement(Object) => HTML(Browser Understand)
  */
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child", key: "child1" }, [
-        React.createElement("h1", { key: "key1" }, "I am a h1 tag!"),
-        React.createElement(
-            "h2",
-            { key: "key2" },
-            "I am a h2 tag under same child node!"
-        ),
-    ]),
+
+const parent = React.createElement("customTag", { id: "parent" }, [
+    React.createElement("div", { id: "child", key: "child1" }, React.createElement("h1", { key: "key1" }, "I am a child1 h1 tag!")),
     React.createElement("div", { id: "child2", key:"child2" }, [
-        React.createElement("h1", { key: "key1" }, "I am a h1 tag!"),
-        React.createElement(
-            "h2",
-            { key: "key2" },
-            "I am a h2 tag under same child node!"
-        ),
+        React.createElement("h1", { key: "key1" }, "I am a child2 h1 tag!"),
+        React.createElement("h2", { key: "key2" }, "I am a child2 h2 tag under same child node!"),
     ]),
 ]);
 
@@ -36,6 +26,7 @@ console.log(parent); // object
 
 // JSX?
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("header"));
 
 root.render(parent);
