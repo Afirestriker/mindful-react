51a57b98054b9fefffb21c93e66ae63656e9f8be
Ashish Agrawal <agrawalashish.in@gmail.com>
2025-04-04 09:34:25 +0530

    doc: add comment for concept understanding

--- App.js
+++ App.js
@@ -1,30 +1,23 @@
 /**
- * <div id="parent">
- *  <div id="child">
- *    <h1></h1>
- *  </div>
- *  <div id="child2">
- *    <h1></h1>
- *    <h1></h1>
- *  </div>
- * </div>
- *
  * ReactElement is an Object => which convert the object into an HTML tag which the Browser's understand
  * ReactElement(Object) => HTML(Browser Understand)
  */
-const parent = React.createElement("customTag", { id: "parent" }, [
-    React.createElement("div", { id: "child", key: "child1" }, React.createElement("h1", { key: "key1" }, "I am a child1 h1 tag!")),
-    React.createElement("div", { id: "child2", key:"child2" }, [
-        React.createElement("h1", { key: "key1" }, "I am a child2 h1 tag!"),
-        React.createElement("h2", { key: "key2" }, "I am a child2 h2 tag under same child node!"),
-    ]),
-]);
+// React.createElement is a JS object
+const parent = React.createElement(
+    "customTag", // which element/tag to be created? Can be anything
+    { id: "parent" }, // setting attributes to the element created
+    [ // nested siblings element in an array
+        React.createElement("div", { id: "child", key: "child1" }, React.createElement("h1", { key: "key1" }, "I am a child1 h1 tag!")),
+        React.createElement("div", { id: "child2", key:"child2" }, [
+            React.createElement("h1", { key: "key1" }, "I am a child2 h1 tag!"),
+            React.createElement("h2", { key: "key2" }, "I am a child2 h2 tag under same child node!"),
+        ]),
+    ]
+);
 
-console.log(parent) // object
+console.log(parent) // view React.createElement JS object structure
 
-// JSX?
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-const root = ReactDOM.createRoot(document.getElementById("header"));
-
+// overwrite the complete `#root` tag ccontent
 root.render(parent);
