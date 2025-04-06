c175ef1033c7bd872d91bfff2f8069735e1c672d
Ashish Agrawal <agrawalashish.in@gmail.com>
2025-04-06 10:18:13 +0530

    feat: ch02-Part02-IgnitingOurApp
    - install parcel
    - replace react cdn link with react node_modules
    - update script type to module
    - explore parcel features
    - explore browserslist
    - update ch02-IgnitingOurApp theory

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
-/**
- * ReactElement is an Object => which convert the object into an HTML tag which the Browser's understand
- * ReactElement(Object) => HTML(Browser Understand)
- */
-// React.createElement is a JS object
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement(
-    "customTag", // which element/tag to be created? Can be anything
-    { id: "parent" }, // setting attributes to the element created
-    [ // nested siblings element in an array
-        React.createElement("div", { id: "child", key: "child1" }, React.createElement("h1", { key: "key1" }, "I am a child1 h1 tag!")),
-        React.createElement("div", { id: "child2", key:"child2" }, [
-            React.createElement("h1", { key: "key1" }, "I am a child2 h1 tag!"),
-            React.createElement("h2", { key: "key2" }, "I am a child2 h2 tag under same child node!"),
-        ]),
+    "div",
+    { id: "parent" },
+    [
+        React.createElement("div", { id: "child", key: "child1" }, React.createElement("h1", { key: "key1" }, "Hello From Mindful React 🚀")),
+        React.createElement("div", { id: "child2", key:"child2" }, React.createElement('h2', {key: 'author'}, 'By Ashish Agrawal')),
     ]
 );
 
-console.log(parent) // view React.createElement JS object structure
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// this overwrite's the complete `#root` tag ccontent
 root.render(parent);
