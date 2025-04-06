397283555c53a348fc0d1ae5ef724eb4af606113
Ashish Agrawal <agrawalashish.in@gmail.com>
2025-04-06 17:12:25 +0530

    feat(ch03-Part03-layingTheFoundation): bye bye React.createElement
    - Will use JSX from now on to create React Element
    - JSX at the end of the day is React Element

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// React.createELement => React Element (JS Object) => HTMLElement(render)
-
-const heading = React.createElement('h1', {id: 'heading'}, 'Mindful React 🚀');
-
-console.log('React.createElement', heading);
-
 // JSX - HTML-Like or XML-Like syntax
 // JSX (transpiled before it reached to JS Engine) - Babel
 // JSX => React.createElement => React Element (JS Object) => HTMLElement(render)
