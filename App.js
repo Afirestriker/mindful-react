6225b69788996f2b283cb728185532be179befec
Ashish Agrawal <agrawalashish.in@gmail.com>
2025-04-07 08:13:36 +0530

    feat(ch03-part04-laying-the-foundation): part04 init commit

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX (transpiled before it reached to JS Engine) - Babel
-// JSX => Babel transpiled it to React.createElement => React Element (JS Object) => HTMLElement(render)
-
-const jsxHeading = (
+// React Element
+const heading = (
   <h1 className="head" tabIndex="1">
     Mindful React from JSX 🚀
   </h1>
 );
 
-console.log(jsxHeading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(heading);
